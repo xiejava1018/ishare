26a5e0c04d0fdd144d3420c5ce9c1150b4eda8e1
xiejava1018 <shygiant@126.com>
2019-08-07 09:42:33 +0800

    ishare-common初始化提交

---
+++ ishare-common/src/main/java/com/ishareread/common/IshareCommonApplication.java
@@ -0,0 +1,13 @@
+package com.ishareread.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IshareCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IshareCommonApplication.class, args);
+	}
+
+}
